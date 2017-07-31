195bfc2bd7c3922bdcfbfaeb9161acddca046427
Tony Gustafsson <mail@tonyg.se>
2017-07-31 18:48:21 +0200

    Add old design

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Interface = ({ loading }) => {
     return (
-        <div>
-            <h2>Eve</h2>
+        <div className="interface">
+            <a href="http://www.tonyg.se/projects/sarabot/" title="Gå till startsidan">
+                <img src="http://www.tonyg.se/projects/sarabot/assets/images/bot.jpg" class="bot-img" alt="Sarabot" />
+            </a>
 
-            <div className="interface">
-                <p>Cool</p>
+            <form method="post" id="dialog_form" class="dialog-form" action="http://www.tonyg.se/projects/sarabot/bot/speak">
+                <input id="input_field" class="input-field" type="text" name="input" />
+                <input type="submit" value="Say" class="btn-submit" />
+                <input type="button" value="Clear" class="btn-clear" id="clear" data-url="http://www.tonyg.se/projects/sarabot/bot/forget" />
+            </form>
+
+            <div id="dialog" class="dialog">
+                <p>&#60;18:35:04&#62; eve: Hi, what's your name?</p>
             </div>
+
+
         </div>
     );
 }
